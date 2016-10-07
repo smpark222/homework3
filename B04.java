f769eb95d523aeea336c5aa18cc4a74e4e8248ef
smpark222 <sungmin567@naver.com>
2016-10-07 10:35:52 +0900

    Update B04.java
    B04.java 수정

---
+++ B04.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class A01 {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		System.out.print("연봉을 입력 : ");
+		int money = input();
+		double result = tax(money);
+		System.out.printf("당신의 소득세는 %.1f원입니다.", result);
+	}
+	
+	public static int input(){
+		Scanner num = new Scanner(System.in);
+		int money =  num.nextInt();
+		return money;
+	}
+	
+	public static double tax(int money){
+		double tax;
+		if(money < 10000000) tax = money * 0.095;
+		else if(money < 40000000) tax = money * 0.19;
+		else if(money < 80000000) tax = money * 0.28;
+		else tax = money * 0.37;
+		return tax;
+	}
+}
