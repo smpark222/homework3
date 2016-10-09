09dba741d49ae4f05c2b73b34967b6da642afac0
smpark222 <sungmin567@naver.com>
2016-10-09 17:05:19 +0900

    A03.java 파일 365일 초과 금지  설정

--- A03.java
+++ A03.java
@@ -1,24 +1,27 @@
 import java.util.*;
-class A02{
+class A03{
 	public static void main(String[] args){
 		System.out.print("월을 입력하세요 ");
 		int month = input();
-		System.out.print("일을 입력하세요");
+		System.out.print("일을 입력하세요 ");
 		int day = input();
 		int day_count = month(month) + day;
-		System.out.printf("이 날짜는 1년 중 %d 번째 날에 해당됩니다..\n", day_count);
-		
+		if(day_count <= 365) System.out.printf("이 날짜는 1년 중 %d 번째 날에 해당됩니다.\n", day_count);
+		else System.out.println("잘못 입력하셨습니다.");
 	}
 
-	public static double input(){
+	public static int input(){
 		Scanner input = new Scanner(System.in);
-		return input.nextDouble();
+		return input.nextInt();
 	}
 
 	public static int month(int month){
-		int month_count = 30 * (month - 1);
-		if(month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10) month_count++;
-		else if (month = 2) month_count -= 2;
+		int month_count = 0;
+		for(int i = 1; i < month; i++){
+			month_count += 30;
+			if(i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10) month_count++;
+			else if (i == 2) month_count -= 2;
+		}
 		return month_count;
 	}
 }
